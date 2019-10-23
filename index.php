bbca7ae7cea582b69f7ca9921ff9d459bc56c94d
Luan <luanmessias10@gmail.com>
2019-10-23 16:00:15 -0300

    Deploy

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 <?php include './partials/home/main_banner.php' ?>
 <?php include './partials/home/loja_completa.php' ?>
 <?php include './partials/home/plataforma_otimizada.php' ?>
+<?php include './partials/home/lojas.php' ?>
 <?php include './partials/global/footer.php' ?>
---
+++ partials/home/lojas.php
@@ -0,0 +1,10 @@
+<div class="dc_wrapper dc_home_03">
+    <div class="dc_wrapper__content">
+
+        <div class="dc_section_title dc_section_title--center">
+            <h1>Clientes </h1>
+            <span class="dc_section_title__small">Em nossos 20 anos de experiência tivemos a oportunidade de fazer parte do sucesso das maiores e melhores operações de e-commerce do Brasil.</span>
+        </div>
+
+    </div>
+</div>
